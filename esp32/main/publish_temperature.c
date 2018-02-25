5a4d010f0dd6389b8ea800e0f9320ac417d0e800
Matthew Berryman <matthew@acrossthecloud.net>
2018-02-25 11:11:29 +1100

    fix published json format

--- esp32/main/publish_temperature.c
+++ esp32/main/publish_temperature.c
@@ -361,7 +361,7 @@ void aws_iot_task(void *param) {
         vTaskDelay(5000 / portTICK_RATE_MS);
         float temperature = getTemperature();
         time_t now; time(&now);
-        sprintf(cPayload, "{\"time\" : \"%lu\", \"temperature\" : \"%.2f\"}", now, temperature);
+        sprintf(cPayload, "{\"time\" : %lu, \"temperature\" : %.2f}", now, temperature);
         paramsQOS0.payloadLen = strlen(cPayload);
         rc = aws_iot_mqtt_publish(&client, TOPIC, TOPIC_LEN, &paramsQOS0);
 
